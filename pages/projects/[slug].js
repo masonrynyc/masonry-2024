7fc7e93598a8c7eb16b955643cc63c9b7981831f
Matt Gordils <mattgordils@gmail.com>
2024-06-30 10:43:09 -0400

    add placeholder section to top of project psge

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -10,6 +10,7 @@ import projectQuery, { allProjects } from '@queries/project'
 import settingsQuery from '@queries/settings'
 import menusQuery from '@queries/menus'
 import NotFoundPage from '@pages/404'
+import Section from '@components/Section'
 import Modules from '@components/Modules'
 import RichText from '@components/RichText'
 import { ProjectGridSmall } from '@components/ProjectGrid'
@@ -37,40 +38,58 @@ export const ProjectContent = ({ data, settings, menus, preview = false }) => {
       menus={menus}
       preview={preview}
     >
-      <div className="px-margin py-v-space max-w-site-max-w mx-auto md:hidden"><h1 className='h5 border-t pt-3'>{project.title}</h1></div>
-      <div className='px-margin grid gap-v-space'>
-        <div>
-          <h2 className='h1'>{project.pageTitle}</h2>
-        </div>
-        <div className="grid md:grid-cols-12 gap-x-gutter">
-          <div className='grid gap-y-6 md:col-span-5'>
-            {project?.projectMeta?.client && (
-              <div className='border-t pt-3'>
-                <h3 className='h5 mb-4'>Client</h3>
-                {project.projectMeta.client}
-              </div>
-            )}
-
-            {project?.projectMeta?.project && (
-              <div className='border-t pt-3'>
-                <h3 className='h5 mb-4'>Project</h3>
-                {project.projectMeta.project}
-              </div>
-            )}
-            
-            {project?.services && project?.services?.length > 0 && (
-              <div className='border-t pt-3'>
-                <h3 className='h5 mb-4'>Services</h3>
-                {project.services.join(', ')}
-              </div>
-            )}
-          </div>
-
-          <div className='border-t pt-3 md:col-span-7'>
-            <RichText text={project.body}/>
+      <Section
+        setTheme='default'
+        prevTheme={false}
+        nextTheme='default'
+        isFirstSection={true}
+      >
+        <div className="px-margin">
+          <div className="text-center h3 rounded bg-[#777] px-margin py-v-space">
+            Intro Media
           </div>
         </div>
-      </div>
+      </Section>
+      <Section
+        setTheme='default'
+        prevTheme='default'
+        nextTheme='default'
+      >
+        <div className="px-margin py-v-space max-w-site-max-w mx-auto md:hidden"><h1 className='h5 border-t pt-3'>{project.title}</h1></div>
+        <div className='px-margin grid gap-v-space'>
+          <div>
+            <h2 className='h1'>{project.pageTitle}</h2>
+          </div>
+          <div className="grid md:grid-cols-12 gap-x-gutter">
+            <div className='grid gap-y-6 md:col-span-5'>
+              {project?.projectMeta?.client && (
+                <div className='border-t pt-3'>
+                  <h3 className='h5 mb-4'>Client</h3>
+                  {project.projectMeta.client}
+                </div>
+              )}
+
+              {project?.projectMeta?.project && (
+                <div className='border-t pt-3'>
+                  <h3 className='h5 mb-4'>Project</h3>
+                  {project.projectMeta.project}
+                </div>
+              )}
+              
+              {project?.services && project?.services?.length > 0 && (
+                <div className='border-t pt-3'>
+                  <h3 className='h5 mb-4'>Services</h3>
+                  {project.services.join(', ')}
+                </div>
+              )}
+            </div>
+
+            <div className='border-t pt-3 md:col-span-7'>
+              <RichText text={project.body}/>
+            </div>
+          </div>
+        </div>
+      </Section>
       <Modules modules={modules} moduleRefs={refModules} />
     </Layout>
   )
